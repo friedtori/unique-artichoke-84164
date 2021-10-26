787abbe34393f90c4de9136a4c7d0cab0608d31e
Stackbit Code Editor <projects@stackbit.com>
2021-10-26 18:11:28 +0000

    next.config.js: changed

--- next.config.js
+++ next.config.js
@@ -21,3 +21,20 @@ module.exports = {
         return config;
     }
 };
+
+module.exports = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      issuer: {
+        test: /\.(js|ts)x?$/,
+       // for webpack 5 use
+       // { and: [/\.(js|ts)x?$/] }
+      },
+      
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
+};
